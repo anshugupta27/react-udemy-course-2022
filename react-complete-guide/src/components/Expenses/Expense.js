997b508dd48eab58b2b8fb88c115f210f2fc7377
Anshu Gupta <rush2anshugupta@gmail.com>
2022-10-06 07:51:14 +0530

    conditional content render

--- react-complete-guide/src/components/Expenses/Expense.js
+++ react-complete-guide/src/components/Expenses/Expense.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./Expense.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -10,20 +10,17 @@ function Expense(props) {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList expenseList={filteredExpenses}/>
     </Card>
   );
 }
