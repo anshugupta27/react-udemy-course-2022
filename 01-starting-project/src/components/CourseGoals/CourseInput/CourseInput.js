00d1d298639c213a8f45592e7c07b8b9d4b91efa
Anshu Gupta <rush2anshugupta@gmail.com>
2022-10-07 13:06:37 +0530

    alternative inline stlyes

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -23,9 +23,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color:isValid?'green':'red'}}>Course Goal</label>
-        <input style={{color:isValid?'green':'red', background:isValid?'white':'pink'}} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${isValid?'':'invalid'}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
