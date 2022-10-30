3c6b4a01f9eaffa28dd08450043bc4bb9bc5dc25
Anshu Gupta <rush2anshugupta@gmail.com>
2022-10-30 08:55:58 +0530

    useContex practical done right

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
-
-const Home = (props) => {
+import AuthContext from '../../context/auth-context';
+import Button from '../UI/Button/Button';
+const Home = () => {
+  const ctx = useContext(AuthContext)
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={ctx.onLogout}>Logout</Button>
     </Card>
   );
 };
